2e5c9b3d0e657a291cf5380b3ee5cbbcbbe91140
abhacid <ab.hacid@hotmail.fr>
2015-06-24 10:31:39 +0200

    ajout de SymBolExtensions et correction de moneyManagement

--- Sources/Library/cAlgoLib/RobotExtensions.cs
+++ Sources/Library/cAlgoLib/RobotExtensions.cs
@@ -17,7 +17,7 @@
 //DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Project Hosting for Open Source Software on Codeplex : https://calgobots.codeplex.com/
+// Project Hosting for Open Source Software on Codeplex : https://github.com/abhacid/cAlgoBot
 #endregion
 
 using cAlgo.API;
@@ -60,7 +60,7 @@ namespace cAlgo.Lib
 			else
 			{
 				double moneyToInvestInDepositCurrency = robot.Account.Balance * risk/ (double)100;
-				double moneyToInvestInQuoteCurrency = moneyToInvestInDepositCurrency / robot.Symbol.PipValue;
+				double moneyToInvestInQuoteCurrency = moneyToInvestInDepositCurrency * robot.Symbol.Mid();
 				double volume = moneyToInvestInQuoteCurrency / stopLossPips;
 				//long normalizedVolume = robot.Symbol.NormalizeVolume(volume);
 
