90915d98afa49ee1c5092c9113d09ff3922d1807
abhacid <ab.hacid@gmail.com>
2015-07-06 00:15:43 +0200

    Ajout de modifications du robot MacdPrbSARnoise

---
+++ Sources/Robots/MACD PrbSAR noise II/MACD PrbSAR noise II/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MACD PrbSAR noise II")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("MACD PrbSAR noise II")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35f19c43-c7cc-4f53-b8e0-61caaf9cd8e8")]
+
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
